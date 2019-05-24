5d39e4ad3f87c03f8a68a2e97a6a8ae03192d051
dilrubajahan <djahanqa@gmail.com>
2019-05-24 16:19:43 -0400

    forloop3

---
+++ src/basic/ForLoopThree.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class ForLoopThree {
+    public static void main(String[] args) {
+        String WeekDays[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+         {
+             for (String w : WeekDays)
+            System.out.println("these are the days of the week:" +w);
+
+
+             for(int macbook=1500; macbook>1200;macbook--){
+                 System.out.println("macbook's price is $1500");
+
+             }
+
+
+        }
+    }
+}
--- src/basic/ForLoops.java
+++ src/basic/ForLoops.java
@@ -2,7 +2,7 @@ package basic;
 
 public class ForLoops {
     public static void main(String[]args){
-       for (int EyeLiner=20;EyeLiner<40;EyeLiner--)
-       System.out.println(40);
+       for (int EyeLiner=20;EyeLiner>10;EyeLiner--)
+       System.out.println("i will buy eyeliners");
     }
 }
