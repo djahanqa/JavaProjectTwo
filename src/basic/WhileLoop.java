ccd98b92f36dbdc109d00efb831d07409759ae9b
dilrubajahan <djahanqa@gmail.com>
2019-05-31 08:09:15 -0400

    while loop

---
+++ src/basic/WhileLoop.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class WhileLoop {
+    public static void main(String[]args){
+
+        int TeddyBear=1;
+        int Barbie=4;
+        while(TeddyBear<Barbie){
+            System.out.println("result="+Barbie);
+            TeddyBear++;
+
+
+        }
+    }
+}
--- src/basic/practice.java
+++ src/basic/practice.java
@@ -1,20 +1,36 @@
-package basic;
+//package basic;
 //{"February", "March", "April", "MaY", "June", "July", "Augest", "September", "October", "November", "December"};
 
-public class practice {
-    public static void main(String[] args) {
-        String year[] = {"first month January","second month February", "third month March", "fourth month April", "fifth month MaY",
+//public class practice {
+   /* public static void main(String[] args) {
+       int house1=0;
+        int house2=4;
+
+      while(house1>house2) {
+          System.out.println("result=" + house2);
+          house2++;
+
+
+      }
+    }//
+
+}
+/*forloop
+String year[] = {"first month January","second month February", "third month March", "fourth month April", "fifth month MaY",
                 "sixth month June", "seventh July", "eighth month August", "nineth month September", "tenth Month October", "eleventh month November",
                 "twelveth month December"};
 
         for (String f : year)
 
-        { System.out.println("I love the " +f);}
+        { System.out.println("I love the " +f);}*/
 
+ /* try catch
+ int house1=0;
+        int house2=4;
+        try {
+           int result =  house1+house2;
+           System.out.println("result"+result );
+       }catch (Exception e){
+           System.out.println(" is not possible"+e );
 
-
-
-
-    }
-
-}
+       */
