9959945e66c85d55e008f7e4ddfd007de57a7d60
dilrubajahan <djahanqa@gmail.com>
2019-06-10 13:56:50 -0400

    method overriding submain

---
+++ src/basic/MethodOverLoding.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class MethodOverLoding {
+    public static void fruits(){
+        String mango="tasty";
+        System.out.println(mango);
+    }
+
+public static void fruits(int apple){
+    System.out.println(apple);
+}
+
+
+}
